aeb2000ecf3f9f2fc4ca105b35a927c041d2a3be
Tõnis <tonislille@gmail.com>
2023-05-19 20:39:25 +0300

    Add program files

---
+++ ProjectSsentifGenie/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectSsentifGenie;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ProjectSsentifGenie/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProjectSsentifGenie;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
