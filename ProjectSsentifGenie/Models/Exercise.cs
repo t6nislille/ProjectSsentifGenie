97e004ec65374e76adb69de9444f4698335eb8b0
Tõnis <tonislille@gmail.com>
2023-05-19 20:52:00 +0300

    Add Exercise Models

---
+++ ProjectSsentifGenie/Models/Exercise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSsentifGenie.Models
+{
+    public class Exercise
+    {
+        public string Name { get; set; }
+        public string GroupType { get; set; }
+        public string Instructions { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
