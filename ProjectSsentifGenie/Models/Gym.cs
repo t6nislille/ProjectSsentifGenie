e0be1804aaaa4d2faffa7e588c60a21526c2f355
Tõnis <tonislille@gmail.com>
2023-06-08 12:03:25 +0300

    Gym model

---
+++ ProjectSsentifGenie/Models/Gym.cs
@@ -0,0 +1,9 @@
+namespace ProjectSsentifGenie.Models
+{
+    public class Gym
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
